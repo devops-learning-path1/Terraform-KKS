b7d5c6b09b1e8730ce369497dcb9490c2a1c53e8
Bandi143 <nagarajubandi016@gmail.com>
2025-02-01 17:02:59 +0530

    adding the workflow for terraform

--- variables.tf
+++ variables.tf
@@ -4,3 +4,30 @@ variable "aws_region" {
     default = "us-east-1"
   
 }
+
+variable "vpc_cidr" {
+    description = "The CIDR block for the VPC"
+    type = string
+  
+}
+
+variable "cluster_name" {
+    description = "Cluster name"
+    type = string
+  
+}
+variable "private_subnets" {
+    description = "private_subnets"
+    type = list(string)  
+  
+}
+variable "availability_zone" {
+    description = "List of availability zone"
+    type = list(string)
+  
+}
+variable "public_subnets" {
+    description = "public_subnets"
+    type = list(string)  
+  
+}
